03e41ff4f4e12e2e3bb7ef4fb26927226ec7a0fc
Andre Bertuzzi <andre@portlouis.inf.br>
2020-10-19 21:41:14 -0300

    Initial Commit

---
+++ exchange_interface.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from datetime import datetime, timedelta
+
+
+class FtxClient:
+    def __init__(self, base_url=None):
+        self._base_url = 'https://ftx.com/api/'
+
+    def get_all_futures(self):
+        url = f'{self._base_url}futures'
+        response = json.loads((requests.get(url)).content)
+        futures_names = [symbol['name']
+                         for symbol in response['result'] if symbol['perpetual'] == True]
+        return futures_names
+
+    # UTC TIME
+    def get_historical_funding_rates(self, future, start, end):
+        url = f'{self._base_url}funding_rates?future={future}&start_time={start}&end_time={end}'
+        print(url)
+        response = json.loads((requests.get(url)).content)
+        funding_rates = response['result']
+        return funding_rates
+
+
+class BinanceClient:
+    def __init__(self, base_url=None):
+        self._base_url = 'https://www.binance.com/fapi/v1/'
+
+    def get_all_futures(self):
+        url = f'{self._base_url}ticker/24hr'
+        response = json.loads((requests.get(url)).content)
+        futures_names = [symbol['symbol'] for symbol in response]
+        return futures_names
+
+    # UTC -3, need ajusts
+    def get_historical_funding_rates(self, future, start, end):
+        url = f'{self._base_url}fundingRate?symbol={future}&startTime={start}&endTime={end}'
+        # print(url)
+        response = json.loads((requests.get(url)).content)
+        funding_rates = [{'time': (datetime.fromtimestamp(item['fundingTime']/1000) + timedelta(hours=3)).strftime(
+            '%Y-%m-%dT%H:%M:%S+00:00'), 'rate': float(item['fundingRate'])} for item in response]
+        return funding_rates
---
+++ funding_rate.py
@@ -0,0 +1,151 @@
+from datetime import datetime, timedelta
+from exchange_interface import FtxClient, BinanceClient
+from logzero import logger
+from time import gmtime, strftime
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib.dates as mdates
+import numpy
+
+import time
+import os
+import settings
+
+# Environment variables
+FUTURES = os.getenv('LIST_OF_FUTURES')
+
+fundings = {}
+
+ftx = FtxClient()
+binance = BinanceClient()
+
+
+def send_message(message):
+    if(send_telegram):
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    logger.info(message)
+
+
+def get_futures():
+    if(FUTURES == 'all'):
+        binance_futures = [future.replace('USDT', '')
+                           for future in binance.get_all_futures()]
+
+        ftx_futures = [future.replace('-PERP', '')
+                       for future in ftx.get_all_futures()]
+        return list(set(binance_futures) & set(ftx_futures))
+    else:
+        return FUTURES.split(',')
+
+
+def plot_funding_return(_futures=[], _start=datetime(2020, 6, 8), _end=datetime.now(), _combined=True):
+    if(len(_futures) == 0):
+        futures = get_futures()
+    else:
+        futures = _futures.copy()
+    # print(futures)
+    dfs = []
+    for future in futures:
+        print(future)
+        ftx_final = []
+        binance_final = []
+        start = _start
+        end = _end
+        print(start)
+        print(end)
+        count = 1
+        while(start < _end):
+            print(count)
+            end = start + timedelta(days=20)
+            ftx_funding_rates = ftx.get_historical_funding_rates(
+                f'{future}-PERP', start.timestamp(), end.timestamp())
+            ftx_funding_rates.reverse()
+            ftx_final = ftx_final + ftx_funding_rates
+            # print('FTX------------')
+            # print(ftx_funding_rates)
+            binance_funding_rates = binance.get_historical_funding_rates(
+                f'{future}USDT', int(start.timestamp()*1000), int(end.timestamp()*1000))
+            # print('BINANCE--------', start, end)
+            # print(binance_funding_rates)
+            binance_final = binance_final + binance_funding_rates
+            count += 1
+            start = end
+            time.sleep(5)
+        if(_combined):
+            if(len(ftx_final) > 10 and len(binance_final) > 1):
+                ftx_df = pd.DataFrame(ftx_final)
+                binance_df = pd.DataFrame(binance_final)
+                df = pd.merge(ftx_df, binance_df, how='outer', on='time')
+                df = df.fillna(0)
+                df['rate'] = df['rate_x'] - df['rate_y']
+                df['acum'] = df['rate'].cumsum()
+                df = df.set_index('time')
+                df = df['acum']
+                df = df * 100
+                df.index = pd.to_datetime(df.index)
+                dfs.append({'data': df, 'name': future})
+        else:
+            if(len(ftx_final) > 1):
+                # print(ftx_final)
+                ftx_df = pd.DataFrame(ftx_final)
+                df = ftx_df.fillna(0)
+                df['acum'] = df['rate'].cumsum()
+                df = df.set_index('time')
+                df = df['acum']
+                df = df * 100
+                df.index = pd.to_datetime(df.index)
+                dfs.append({'data': df, 'name': f'{future}-PERP'})
+                binance_df = pd.DataFrame(binance_final)
+                df = binance_df.fillna(0)
+                df['acum'] = df['rate'].cumsum()
+                df = df.set_index('time')
+                df = df['acum']
+                df = df * 100
+                df.index = pd.to_datetime(df.index)
+                dfs.append({'data': df, 'name': f'{future}-USDT'})
+    generate_chart(dfs, False)
+
+
+def generate_chart(dfs, save=False):
+    # Set the locator
+    locator = mdates.MonthLocator()  # every month
+    # Specify the format - %b gives us Jan, Feb...
+    fmt = mdates.DateFormatter('%b')
+    if(save):
+        for item in dfs:
+            plt.plot(item['data'], label=item['name'])
+            plt.legend(loc='lower left')
+            plt.title('FTX Funding Rate Acum', fontsize='16')
+            plt.ylabel('Funding Rate (%)')
+            plt.xlabel('Time')
+            X = plt.gca().xaxis
+            X.set_major_locator(locator)
+            X.set_major_formatter(fmt)
+            plt.savefig(f'output/{item["name"]}')
+            plt.close()
+    else:
+        for item in dfs:
+            plt.plot(item['data'], label=item['name'])
+        plt.legend(loc='lower left')
+        plt.title(f'FTX Funding Rate Acum {item["name"]}', fontsize='16')
+        plt.ylabel('Funding Rate (%)')
+        plt.xlabel('Time')
+        X = plt.gca().xaxis
+        X.set_major_locator(locator)
+        X.set_major_formatter(fmt)
+        plt.show()
+        time.sleep(10)
+        plt.close()
+
+
+# ['FLM', 'DOGE', 'SXP', 'UNI', 'BAL', 'XRP', 'ADA', 'BNB', 'KNC', 'TOMO', 'LINK', 'OMG', 'RUNE', 'XTZ', 'SOL', 'BCH', 'AVAX',
+# 'NEO', 'DOT', 'AAVE', 'ATOM', 'BTC', 'DEFI', 'YFI', 'HNT', 'EOS', 'MKR', 'COMP', 'TRX', 'SUSHI', 'ZEC', 'VET', 'ALGO', 'ETH', 'LTC', 'THETA', 'ETC']
+plot_funding_return(['COMP'],
+                    datetime(2020, 6, 8), datetime.now(), False)
+
+# while True:
+#     start_time = time.time()
+#     get_top_bottom()
+#     execution_time = time.time() - start_time
+#     # Discount execution time from DELAY
+#     time.sleep(DELAY - int(execution_time))
