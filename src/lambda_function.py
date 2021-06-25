317ddddce5bbce9d27c62384aa15b6965a567edf
Andre Bertuzzi <andreqbertuzzi@gmail.com>
2021-06-25 17:48:17 -0300

    Lambda in paralel

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,11 +1,19 @@
 import time
+
+from requests.api import get
 from services import Services
 from exchanges import FtxClient, PerpetualClient, BinanceUSDClient, BinanceUSDTClient
 from datetime import date, datetime, timedelta
 import threading
 import json
+import boto3
 
 services = Services()
+ftx = FtxClient()
+busdt = BinanceUSDTClient()
+busd = BinanceUSDClient()
+perpetual = PerpetualClient()
+
 
 def save_fundings(client, future, fundings):
     for funding in fundings:
@@ -19,7 +27,25 @@ def get_common_assets(client1, client2):
     return list(set(assets1) & set(assets2))
 
 
-def call_exchange(client, start, end, assets):
+def get_assets():
+    return list(set(get_common_assets(ftx, busdt) + get_common_assets(ftx, busd) + get_common_assets(ftx, perpetual) +
+                get_common_assets(busdt, busd) + get_common_assets(busdt,
+                                                                   perpetual) + get_common_assets(busd, perpetual)))
+
+
+def call_exchange(client, start, end, assets, increment):
+    """Summary or Description of the Function
+
+    Parameters:
+    client (Exchange): Exchange interface client
+    start (int): Start date
+    end (datetime): End date
+    assets (list): List os assets 
+    increment (int): Number of days
+
+    Returns:
+    int:Returning value
+    """
     futures_total = list(map(client.parse, assets))
     futures_client = client.get_all_futures()
     futures = list(set(futures_total) & set(futures_client))
@@ -28,7 +54,7 @@ def call_exchange(client, start, end, assets):
         temp_start = start
         temp_end = end
         while(temp_start < end):
-            temp_end = temp_start + timedelta(days=20)
+            temp_end = temp_start + timedelta(days=increment)
             print(f'{client.name} {future} Start {temp_start} Ending {temp_end}')
             funding_rates = client.get_historical_funding_rates(
                 future, temp_start.timestamp(), temp_end.timestamp())
@@ -37,23 +63,21 @@ def call_exchange(client, start, end, assets):
             time.sleep(5)
 
 
-def get_all_fundings(last_date):
+def get_all_fundings_thread(last_date):
     """Summary or Description of the Function
-    
+
     Parameters:
     last_date (datetime): Last date when method ran, use to be yesterday
-    
+
     Returns:
     """
-    
+
     ftx = FtxClient()
     busdt = BinanceUSDTClient()
     busd = BinanceUSDClient()
     perpetual = PerpetualClient()
 
-    assets = list(set(get_common_assets(ftx, busdt) + get_common_assets(ftx, busd) + get_common_assets(ftx, perpetual) +
-                    get_common_assets(busdt, busd) + get_common_assets(busdt,
-                                                                        perpetual) + get_common_assets(busd, perpetual)))
+    assets = get_assets()
 
     exchanges = [ftx, busdt, busd, perpetual]
 
@@ -61,22 +85,58 @@ def get_all_fundings(last_date):
     end = datetime.utcnow()
 
     threads = [threading.Thread(target=call_exchange, args=(
-        client, start, end, assets)) for client in exchanges]
+        client, start, end, assets, 3)) for client in exchanges]
     [thread.start() for thread in threads]
 
 # [call_exchange(client, start, end, assets) for client in exchanges]
 
 
-def lambda_handler(event, context):
-    message='All funding rates saved'
-    try:
-        last_date = services.get_load_date()
-        print(last_date)
-        get_all_fundings(last_date)
-    except:
-        message='Error trying to get funding rates'
+def get_all_fundings_by_asset(last_date, asset):
+    """Summary or Description of the Function
 
-    return { 
+    Parameters:
+    last_date (datetime): Last date when method ran, use to be yesterday
+
+    Returns:
+    """
+    exchanges = [ftx, busdt, busd, perpetual]
+
+    start = last_date
+    end = datetime.utcnow()
+    [call_exchange(client, start, end, [asset], 3) for client in exchanges]
+
+
+def call_lambda(asset):
+    client = boto3.client('lambda')
+    time.sleep(5)
+    client.invoke(
+        FunctionName='crypto-consumer',
+        InvocationType='Event',
+        LogType='None',
+        Payload=json.dumps({'asset': asset}))
+
+
+def lambda_handler(event, context):
+    asset = event.get('asset')
+    try:
+        if(asset):
+            last_date = services.get_load_date()
+            get_all_fundings_by_asset(last_date, asset)
+            message = f'All {asset} funding rates saved'
+        else:
+            message = f'All funding rates saved'
+            assets = get_assets()
+            last_date = services.get_load_date()
+            [call_lambda for asset in assets]
+
+            # result = json.loads(response['Payload'].read().decode("utf-8"))
+    except:
+        message = 'Error trying to get funding rates'
+
+    return {
         'statusCode': 200,
         'body': json.dumps(message)
     }
+
+
+# lambda_handler({}, None)
