85fe4fbfd7cf79c55f36c3f64322a9bd5518543a
Andre Bertuzzi <andreqbertuzzi@gmail.com>
2021-07-07 00:00:43 -0300

    Added Binance Futures USDT

--- src/exchanges.py
+++ src/exchanges.py
@@ -182,9 +182,20 @@ class FtxClient(Exchange):
 
 
 class BinanceUSDTClient(Exchange):
-    def __init__(self, base_url=None):
+    def __init__(self, key=None, secret=None, base_url=None):
         self._base_url = 'https://www.binance.com/fapi/v1/'
         self.name = 'BINANCE-USDT'
+        self._key = key
+        self._secret = secret
+        self.session = self._init_session()
+
+    def _init_session(self):
+
+        session = requests.session()
+        session.headers.update({'Accept': 'application/json',
+                                'User-Agent': 'binance/python',
+                                'X-MBX-APIKEY': self._key})
+        return session
 
     @staticmethod
     def parse(asset):
@@ -208,6 +219,132 @@ class BinanceUSDTClient(Exchange):
             '%Y-%m-%dT%H:%M:%S+00:00'), 'rate': float(item['fundingRate'])} for item in response]
         return funding_rates
 
+    def futures_income_history(self, **params):
+        """Get income history for authenticated account
+
+        https://binance-docs.github.io/apidocs/futures/en/#get-income-history-user_data
+
+        """
+        
+        return self._request_futures_api('get', 'income', True, data=params)
+
+    def _request_futures_api(self, method, path, signed=False, **kwargs):
+        uri = 'https://fapi.binance.com/fapi/v1/' + path
+
+        return self._request(method, uri, signed, True, **kwargs)
+    
+    def _create_website_uri(self, path):
+        return self.WEBSITE_URL + '/' + path
+
+    def _order_params(self, data):
+        """Convert params to list with signature as last element
+
+        :param data:
+        :return:
+
+        """
+        has_signature = False
+        params = []
+        for key, value in data.items():
+            if key == 'signature':
+                has_signature = True
+            else:
+                params.append((key, value))
+        # sort parameters by key
+        params.sort(key=itemgetter(0))
+        if has_signature:
+            params.append(('signature', data['signature']))
+        return params
+
+    def _request(self, method, uri, signed, force_params=False, **kwargs):
+
+        # set default requests timeout
+        kwargs['timeout'] = 10
+
+        # add our global requests params
+        # if self._requests_params:
+        #     kwargs.update(self._requests_params)
+
+        data = kwargs.get('data', None)
+        if data and isinstance(data, dict):
+            kwargs['data'] = data
+
+            # find any requests params passed and apply them
+            if 'requests_params' in kwargs['data']:
+                # merge requests params into kwargs
+                kwargs.update(kwargs['data']['requests_params'])
+                del(kwargs['data']['requests_params'])
+
+        if signed:
+            # generate signature
+            kwargs['data']['timestamp'] = int(_time() * 1000)
+            kwargs['data']['signature'] = self._generate_signature(kwargs['data'])
+
+        # sort get and post params to match signature order
+        if data:
+            # sort post params
+            kwargs['data'] = self._order_params(kwargs['data'])
+            # Remove any arguments with values of None.
+            null_args = [i for i, (key, value) in enumerate(kwargs['data']) if value is None]
+            for i in reversed(null_args):
+                del kwargs['data'][i]
+
+        # if get request assign data array to params value for requests lib
+        if data and (method == 'get' or force_params):
+            kwargs['params'] = '&'.join('%s=%s' % (data[0], data[1]) for data in kwargs['data'])
+            del(kwargs['data'])
+
+        self.response = getattr(self.session, method)(uri, **kwargs)
+        return self._handle_response()
+    
+    def _handle_response(self):
+        """Internal helper for handling API responses from the Binance server.
+        Raises the appropriate exceptions when necessary; otherwise, returns the
+        response.
+        """
+        if not str(self.response.status_code).startswith('2'):
+            pass
+        try:
+            return self.response.json()
+        except ValueError:
+            pass
+
+    def _generate_signature(self, data):
+        ordered_data = self._order_params(data)
+        query_string = '&'.join(["{}={}".format(d[0], d[1]) for d in ordered_data])
+        m = hmac.new(self._secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256)
+        return m.hexdigest()
+
+class itemgetter:
+    """
+    Return a callable object that fetches the given item(s) from its operand.
+    After f = itemgetter(2), the call f(r) returns r[2].
+    After g = itemgetter(2, 5, 3), the call g(r) returns (r[2], r[5], r[3])
+    """
+    __slots__ = ('_items', '_call')
+
+    def __init__(self, item, *items):
+        if not items:
+            self._items = (item,)
+            def func(obj):
+                return obj[item]
+            self._call = func
+        else:
+            self._items = items = (item,) + items
+            def func(obj):
+                return tuple(obj[i] for i in items)
+            self._call = func
+
+    def __call__(self, obj):
+        return self._call(obj)
+
+    def __repr__(self):
+        return '%s.%s(%s)' % (self.__class__.__module__,
+                              self.__class__.__name__,
+                              ', '.join(map(repr, self._items)))
+
+    def __reduce__(self):
+        return self.__class__, self._items
 
 class BinanceUSDClient(Exchange):
     def __init__(self, base_url=None):
