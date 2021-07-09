9cb88eddd072de3c1fde4b4ead0e6e8cafd2518c
Andre Bertuzzi <andreqbertuzzi@gmail.com>
2021-07-08 21:33:21 -0300

    Generic method to save data

--- src/services.py
+++ src/services.py
@@ -4,9 +4,10 @@ from utils.db import DataBase
 
 
 class Services:
-    def save_funding(self, exchange, date, asset, future, type, rate):
-        sql = '''insert into crypto.fundings(exchange, date, asset, future, type, rate) values ($1, $2, $3, $4, $5, $6);'''
-        args = [exchange, date, asset, future, type, rate]
+    def save_trade(self, exchange, date, asset, future, type, rate, notional=0, payment=0):
+        sql = '''insert into crypto.fundings(exchange, date, asset, future, type, rate, notional, payment) values ($1, $2, $3, $4, $5, $6, $7, $8);'''
+        args = [exchange, date, asset, future, type, rate, notional, payment]
+        print(sql, args)
         try:
             return DataBase.query(sql, args)
         except Exception as e:
@@ -16,7 +17,8 @@ class Services:
         sql = '''select date(max(date)) as last_date from crypto.fundings;'''
         try:
             result = DataBase.query(sql, None)
-            date_str = (result.get('rows')[0]).get('last_date', '1900-01-01').split('T')[0]
+            date_str = (result.get('rows')[0]).get(
+                'last_date', '1900-01-01').split('T')[0]
             last_date = datetime.strptime(date_str, '%Y-%m-%d')
             return last_date
         except Exception as e:
