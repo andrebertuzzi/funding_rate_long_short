f52e31848c760d494b5a179c37189a9f797e5e5f
Andre Bertuzzi <andreqbertuzzi@gmail.com>
2021-07-08 21:34:00 -0300

    Saving funding payments

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -20,12 +20,20 @@ BINANCE_SECRET = os.getenv('BINANCE_SECRET')
 FTX_KEY = os.getenv('FTX_KEY')
 FTX_SECRET = os.getenv('FTX_SECRET')
 FTX_SUBACC = os.getenv('FTX_SUBACC')
-# ftx_client = Client_FTX(FTX_KEY, FTX_SECRET, 'Ampl', 30)
 
-def save_fundings(client, future, fundings):
-    for funding in fundings:
-        services.save_funding(client.name, funding.get('time'), client.get_asset(
-            future), future, 'FUNDING', funding.get('rate'))
+
+def _get_current_positions():
+    return [{'asset': 'BAND', 'notional': 100, 'type': 'ARB', 'exchanges': ['FTX', 'BINANCEUSDT']},
+            {'asset': 'ETH', 'notional': 10000, 'type': 'ARB', 'exchanges': ['PERP', 'BINANCEUSDT']}]
+
+
+def _get_exchanges():
+    return [ftx, busdt, busd, perpetual]
+
+
+def save_fundings(client, future, fundings, type):
+    [services.save_trade(client.name, funding.get('time'), client.get_asset(
+        future), future, type, funding.get('rate'), funding.get('notional', 0), funding.get('payment', 0)) for funding in fundings]
 
 
 def get_common_assets(client1, client2):
@@ -34,13 +42,13 @@ def get_common_assets(client1, client2):
     return list(set(assets1) & set(assets2))
 
 
-def get_assets():
+def _get_assets():
     return list(set(get_common_assets(ftx, busdt) + get_common_assets(ftx, busd) + get_common_assets(ftx, perpetual) +
                 get_common_assets(busdt, busd) + get_common_assets(busdt,
                                                                    perpetual) + get_common_assets(busd, perpetual)))
 
 
-def call_exchange(client, start, end, assets, increment):
+def call_exchange(client, start, end, assets, increment, function, type):
     """Summary or Description of the Function
 
     Parameters:
@@ -57,15 +65,17 @@ def call_exchange(client, start, end, assets, increment):
     futures_client = client.get_all_futures()
     futures = list(set(futures_total) & set(futures_client))
 
+    print(futures)
+
     for future in futures:
         temp_start = start
         temp_end = end
         while(temp_start < end):
             temp_end = temp_start + timedelta(days=increment)
             print(f'{client.name} {future} Start {temp_start} Ending {temp_end}')
-            funding_rates = client.get_historical_funding_rates(
+            funding_rates = function(
                 future, temp_start.timestamp(), temp_end.timestamp())
-            save_fundings(client, future, funding_rates)
+            save_fundings(client, future, funding_rates, type)
             temp_start = temp_end
             time.sleep(5)
 
@@ -78,18 +88,36 @@ def get_all_fundings_thread(last_date):
 
     Returns:
     """
-    assets = get_assets()
-
-    exchanges = [ftx, busdt, busd, perpetual]
+    assets = _get_assets()
+    exchanges = _get_exchanges()
 
     start = last_date
     end = datetime.utcnow()
 
     threads = [threading.Thread(target=call_exchange, args=(
-        client, start, end, assets, 3)) for client in exchanges]
+        client, start, end, assets, 3, client.get_historical_funding_rates, 'FUNDING')) for client in exchanges]
     [thread.start() for thread in threads]
 
 # [call_exchange(client, start, end, assets) for client in exchanges]
+
+
+def get_all_payments_thread(last_date):
+    """Summary or Description of the Function
+
+    Parameters:
+    last_date (datetime): Last date when method ran, use to be yesterday
+
+    Returns:
+    """
+    assets = [x.get('asset') for x in _get_current_positions()]
+    exchanges = [binance_client]
+
+    start = last_date
+    end = datetime.utcnow()
+
+    threads = [threading.Thread(target=call_exchange, args=(
+        client, start, end, assets, 3, client.get_private_funding_payments, 'PAYMENTS')) for client in exchanges]
+    [thread.start() for thread in threads]
 
 
 def get_all_fundings_by_asset(last_date, asset):
@@ -126,7 +154,7 @@ def lambda_handler(event, context):
             message = f'All {asset} funding rates saved'
         else:
             message = f'All funding rates saved'
-            assets = get_assets()
+            assets = _get_assets()
             last_date = services.get_load_date()
             [call_lambda for asset in assets]
 
@@ -140,6 +168,13 @@ def lambda_handler(event, context):
     }
 
 
+ftx_client = FtxClient(FTX_KEY, FTX_SECRET, FTX_SUBACC, 30)
+binance_client = BinanceUSDTClient(BINANCE_KEY, BINANCE_SECRET)
+
+end = datetime(2021, 7, 5)
+begin = datetime(2021, 7, 1)
+
+get_all_payments_thread(begin)
 # lambda_handler({}, None)
-last_date = services.get_load_date()
-get_all_fundings_thread(last_date)
+# last_date = services.get_load_date()
+# get_all_fundings_thread(begin)
